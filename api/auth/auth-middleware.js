d98e2e5786c0f8d3936c63f5287347db55a9511c
Peter Conley <peterdavidconley@gmail.com>
2022-02-22 18:08:59 -0500

    restricted auth-middleware is working

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require('../users/users-model');
+const jwt = require('jsonwebtoken');
 const restricted =  (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -17,7 +18,19 @@ const restricted =  (req, res, next) => {
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
 
+    const token = req.headers.authorization
+    if (!token) {
 
+      return next({ status: 401, message: 'Token required'})
+
+    } jwt.verify(token, JWT_SECRET, (err, ) => {
+        if (err) {
+            next({ status: 401, message: 'Token invalid' })
+        } else {
+          req.decodedToken = decodedToken
+          next()
+        }
+    })
  
 }
 
